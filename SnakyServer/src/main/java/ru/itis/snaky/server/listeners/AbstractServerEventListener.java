46247b2a3072eb07461d07d78c61b5fd9a029d77
Zl4ve <mudarisov.renat58@gmail.com>
2022-12-21 01:20:04 +0300

    Added case for auth message

--- SnakyServer/src/main/java/ru/itis/snaky/server/listeners/AbstractServerEventListener.java
+++ SnakyServer/src/main/java/ru/itis/snaky/server/listeners/AbstractServerEventListener.java
@@ -1,18 +1,29 @@
 package ru.itis.snaky.server.listeners;
 
+import ru.itis.snaky.protocol.exceptions.ProtocolIllegalMessageTypeException;
 import ru.itis.snaky.protocol.message.MessageType;
+import ru.itis.snaky.server.core.Server;
 
 public abstract class AbstractServerEventListener implements ServerEventListener {
     private byte value;
+
+    protected Server server;
 
     public AbstractServerEventListener(byte value) {
         this.value = value;
     }
 
+    @Override
+    public void init(Server server) {
+        this.server = server;
+    }
+
     public static ServerEventListener get(MessageType messageType) {
         switch (messageType) {
-            // todo
+            case AUTHORIZATION:
+                return new PlayerAuthorizationListener();
+            default:
+                throw new ProtocolIllegalMessageTypeException();
         }
-        return null;
     }
 }
