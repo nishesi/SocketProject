58d8e604f2bcb028221252f0c4c68e7ca20ed3f4
Nurislam Zaripov <znurislama56@gmail.com>
2022-12-24 13:45:34 +0300

    refactor protocol exceptions

--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/exceptions/ProtocolIllegalMessageTypeException.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/exceptions/ProtocolIllegalMessageTypeException.java
@@ -2,22 +2,11 @@ package ru.itis.snaky.protocol.exceptions;
 
 public class ProtocolIllegalMessageTypeException extends RuntimeException {
 
-    public ProtocolIllegalMessageTypeException() {
-    }
-
     public ProtocolIllegalMessageTypeException(String message) {
         super(message);
     }
 
     public ProtocolIllegalMessageTypeException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public ProtocolIllegalMessageTypeException(Throwable cause) {
-        super(cause);
-    }
-
-    public ProtocolIllegalMessageTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/exceptions/ProtocolSerializationException.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/exceptions/ProtocolSerializationException.java
@@ -1,8 +1,6 @@
 package ru.itis.snaky.protocol.exceptions;
 
 public class ProtocolSerializationException extends RuntimeException {
-    public ProtocolSerializationException() {
-    }
 
     public ProtocolSerializationException(String message) {
         super(message);
@@ -10,13 +8,5 @@ public class ProtocolSerializationException extends RuntimeException {
 
     public ProtocolSerializationException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public ProtocolSerializationException(Throwable cause) {
-        super(cause);
-    }
-
-    public ProtocolSerializationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
