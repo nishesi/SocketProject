f8031610482d7e1acc089231d9c0b141a63469b2
Nurislam Zaripov <znurislama56@gmail.com>
2022-12-24 19:44:56 +0300

    add annotations

--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/message/parameters/RoomsListParams.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/message/parameters/RoomsListParams.java
@@ -1,7 +1,15 @@
 package ru.itis.snaky.protocol.message.parameters;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import ru.itis.snaky.protocol.dto.TransferRoom;
 
+@Getter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class RoomsListParams extends MessageParams {
     private TransferRoom[] rooms;
 }
--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/message/parameters/StartParams.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/message/parameters/StartParams.java
@@ -1,5 +1,14 @@
 package ru.itis.snaky.protocol.message.parameters;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class StartParams extends MessageParams {
     private boolean success;
 }
