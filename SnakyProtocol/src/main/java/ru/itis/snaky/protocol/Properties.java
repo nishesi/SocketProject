73e7d79ff3fe67552f86eaa8c96281ae459b9faf
Nurislam Zaripov <znurislama56@gmail.com>
2022-12-25 22:07:34 +0300

    remake structure of serializer

--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/Properties.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/Properties.java
@@ -2,5 +2,5 @@ package ru.itis.snaky.protocol;
 
 public class Properties {
     public static final byte PROTOCOL_VERSION = 2;
-    public static final byte SERIALIZATION_VERSION = 1;
+    public static final byte SERIALIZATION_VERSION = 2;
 }
--- SnakyProtocol/src/main/java/ru/itis/snaky/protocol/serializer/Serializer.java
+++ SnakyProtocol/src/main/java/ru/itis/snaky/protocol/serializer/Serializer.java
@@ -42,31 +42,82 @@ public class Serializer {
     public static <T> void serialize(OutputStream out, T element) {
         try {
             out.write(SERIALIZATION_VERSION);
-            writeElement(out, element);
+            writeElement(out, element, element.getClass());
         } catch (IOException e) {
             throw new ProtocolSerializationException("serialization failed", e);
         }
     }
 
-    private static <T> void writeElement(OutputStream out, T element) throws IOException {
+    private static <T> void writeElement(OutputStream out, T element, Class<?> elementClass) throws IOException {
         validate(out, element);
-        validate(element.getClass());
 
+        if (elementClass.isPrimitive()) {
+            writePrimitive(out, element, elementClass);
+        } else if (elementClass.isArray()) {
+            writeArray(out, element);
+        } else {
+            writeObject(out, element);
+        }
+    }
+
+    private static <T> void writePrimitive(OutputStream out, T element, Class<?> tClass) throws IOException {
+        if (tClass.equals(Boolean.TYPE)) {
+            out.write((byte) ((boolean)element ? 1 : 0));
+
+        } else if (tClass.equals(Byte.TYPE)) {
+            out.write((byte)element);
+
+        } else if (tClass.equals(Short.TYPE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(2).putShort((short)element);
+            out.write(buffer.array());
+
+        } else if (tClass.equals(Integer.TYPE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(4).putInt((int)element);
+            out.write(buffer.array());
+
+        } else {
+            throw new ProtocolSerializationException("Unsupported type: " + tClass);
+        }
+    }
+
+    private static <T> void writeArray(OutputStream out, T array) throws IOException {
+        int length = Array.getLength(array);
+        ByteBuffer buffer = ByteBuffer.allocate(4).putInt(length);
+        out.write(buffer.array());
+
+        for (int i = 0; i < length; i++) {
+            writeElement(out, Array.get(array, i), array.getClass().getComponentType());
+        }
+    }
+
+    private static <T> void writeObject(OutputStream out, T element) throws IOException {
+        if (element.getClass().equals(String.class)) {
+            String str = (String)element;
+            byte[] strBytes = str.getBytes(StandardCharsets.UTF_8);
+            ByteBuffer lenBuffer = ByteBuffer.allocate(4).putInt(strBytes.length);
+
+            out.write(lenBuffer.array());
+            out.write(strBytes);
+            return;
+        }
         Field[] fields = getFilteredSortedFields(element.getClass().getDeclaredFields());
 
-        try {
-            for (Field field : fields) {
-                writeField(out, element, field);
-            }
-        } catch (IllegalAccessException e) {
-            throw new ProtocolSerializationException("Access failed", e);
+        for (Field field : fields) {
+            writeField(out, element, field);
         }
     }
 
 
     private static <T> void validate(OutputStream out, T element) {
-        if (out == null || element == null) {
-            throw new NullPointerException("Can't serialize: out = " + out + ", element = " + element);
+        String mess = null;
+        if (out == null) {
+            mess = "outputStream is null";
+        }
+        if (element == null) {
+            mess += " element is null";
+        }
+        if (mess != null) {
+            throw new NullPointerException("Can't serialize: " + mess);
         }
     }
 
@@ -86,81 +137,32 @@ public class Serializer {
                 .toArray(Field[]::new);
     }
 
-    private static <T> void writeField(OutputStream out, T element, Field field)
-            throws IOException, IllegalAccessException {
+    private static <T> void writeField(OutputStream out, T element, Field field) throws IOException {
+        try {
+            field.setAccessible(true);
+            validateField(field.get(element));
+            writeElement(out, field.get(element), field.getType());
+            field.setAccessible(false);
 
-        field.setAccessible(true);
-        validateField(field.get(element));
-
-        if (field.getType().isPrimitive()) {
-            writePrimitive(out, element, field);
-
-        } else if (field.getType().isArray()) {
-            writeArray(out, element, field);
-
-        } else {
-            writeObject(out, element, field);
-        }
-        field.setAccessible(false);
-    }
-
-    private static <T> void writePrimitive(OutputStream out, T element, Field field)
-            throws IllegalAccessException, IOException {
-
-        Class<?> type = field.getType();
-
-        if (type.equals(Boolean.TYPE)) {
-            out.write((byte) (field.getBoolean(element) ? 1 : 0));
-
-        } else if (type.equals(Byte.TYPE)) {
-            out.write(field.getByte(element));
-
-        } else if (type.equals(Short.TYPE)) {
-
-            short s = field.getShort(element);
-            ByteBuffer buffer = ByteBuffer.allocate(2).putShort(s);
-            out.write(buffer.array());
-
-        } else if (type.equals(Integer.TYPE)) {
-
-            int i = field.getInt(element);
-            ByteBuffer buffer = ByteBuffer.allocate(4).putInt(i);
-            out.write(buffer.array());
-
-        } else if (type.equals(Double.TYPE) || type.equals(Float.TYPE) ||
-                type.equals(Character.TYPE) || type.equals(Long.TYPE)) {
-            throw new ProtocolSerializationException("Unsupported type: " + type);
+        } catch (IllegalAccessException ex) {
+            throw new ProtocolSerializationException("access failed", ex);
         }
     }
 
-    private static <T> void writeArray(OutputStream out, T element, Field field)
-            throws IllegalAccessException, IOException {
-
-        Object arr = field.get(element);
-        int len = Array.getLength(arr);
-
-        ByteBuffer lenBuffer = ByteBuffer.allocate(4).putInt(len);
-        out.write(lenBuffer.array());
-
-        for (int i = 0; i < len; i++) {
-            writeElement(out, Array.get(arr, i));
-        }
-    }
-
-    private static <T> void writeObject(OutputStream out, T element, Field field)
-            throws IllegalAccessException, IOException {
-
-        if (field.getType().equals(String.class)) {
-
-            String str = (String) field.get(element);
-            ByteBuffer lenBuffer = ByteBuffer.allocate(4).putInt(str.length());
-            out.write(lenBuffer.array());
-            out.write(str.getBytes());
-
-        } else {
-            writeElement(out, field.get(element));
-        }
-    }
+//    private static <T> void writeObject(OutputStream out, T element, Field field)
+//            throws IllegalAccessException, IOException {
+//
+//        if (field.getType().equals(String.class)) {
+//
+//            String str = (String) field.get(element);
+//            ByteBuffer lenBuffer = ByteBuffer.allocate(4).putInt(str.length());
+//            out.write(lenBuffer.array());
+//            out.write(str.getBytes());
+//
+//        } else {
+//            writeElement(out, field.get(element));
+//        }
+//    }
 
     public static <T> T deserialize(byte[] arr, Class<T> tClass) {
         ByteArrayInputStream in = new ByteArrayInputStream(arr);
@@ -196,31 +198,81 @@ public class Serializer {
                     "current: " + SERIALIZATION_VERSION +
                     ", required: " + version);
         }
-
-
     }
 
     private static <T> T readElement(InputStream in, Class<T> tClass) throws IOException {
-        validate(tClass);
-        Field[] fields = getFilteredSortedFields(tClass.getDeclaredFields());
-        T obj = createEmptyObject(tClass);
-        try {
-            for (Field field : fields) {
-                injectField(in, field, obj);
-            }
-            return obj;
-        } catch (IllegalAccessException e) {
-            throw new ProtocolSerializationException("Access failed", e);
+        if (tClass.isPrimitive()) {
+            return readPrimitive(in, tClass);
+
+        } else if (tClass.isArray()) {
+            return readArray(in, tClass);
+
+        } else {
+            return readObject(in, tClass);
         }
     }
 
-    private static <T> T createEmptyObject(Class<T> tClass) {
-        //todo ватапук
-        if (tClass.isArray()) {
-            return null;
+    private static <T> T readPrimitive(InputStream in, Class<T> primitiveClass) throws IOException {
+        DataInputStream dataIn = new DataInputStream(in);
+
+        if (primitiveClass.equals(Boolean.TYPE)) {
+            return (T) Boolean.valueOf(dataIn.readBoolean());
+
+        } else if (primitiveClass.equals(Byte.TYPE)) {
+            return (T) Byte.valueOf(dataIn.readByte());
+
+        } else if (primitiveClass.equals(Short.TYPE)) {
+            return (T) Short.valueOf(dataIn.readShort());
+
+        } else if (primitiveClass.equals(Integer.TYPE)) {
+            return (T) Integer.valueOf(dataIn.readInt());
+
+        } else {
+            throw new ProtocolSerializationException("Unsupported type: " + primitiveClass);
         }
-        final Constructor<T> constr = (Constructor<T>) tClass.getConstructors()[0];
-        final List<Object> params = new ArrayList<>();
+    }
+
+    private static <T> T readArray(InputStream in, Class<T> arrayClass) throws IOException {
+        int length = in.read() << 24 |
+                in.read() << 16 |
+                in.read() << 8 |
+                in.read();
+
+        Object array = Array.newInstance(arrayClass.getComponentType(), length);
+        for (int i = 0; i < length; i++) {
+            Array.set(array, i, readElement(in, arrayClass.getComponentType()));
+        }
+        return (T) array;
+    }
+
+    private static <T> T readObject(InputStream in, Class<T> objectClass) throws IOException {
+        if (objectClass.equals(String.class)) {
+            int length = in.read() << 24 |
+                    in.read() << 16 |
+                    in.read() << 8 |
+                    in.read();
+            byte[] strBytes = new byte[length];
+            in.read(strBytes);
+            String str = new String(strBytes, StandardCharsets.UTF_8);
+            return (T)str;
+        }
+
+        T obj = createEmptyObject(objectClass);
+        Field[] fields = getFilteredSortedFields(objectClass.getDeclaredFields());
+
+        for (Field field : fields) {
+            injectField(in, field, obj);
+        }
+        return obj;
+    }
+
+    private static <T> T createEmptyObject(Class<T> tClass) {
+        if (tClass.isArray() || tClass.isPrimitive()) {
+            throw new IllegalArgumentException("illegal type");
+        }
+        Constructor<?>[] constructors = tClass.getConstructors();
+        Constructor<T> constr = (Constructor<T>) constructors[0];
+        List<Object> params = new ArrayList<>();
         for (Class<?> pType : constr.getParameterTypes()) {
             if (pType.equals(Boolean.TYPE)) {
                 params.add(false);
@@ -237,89 +289,39 @@ public class Serializer {
         }
     }
 
-    private static <T> void injectField(InputStream in, Field field, T element)
-            throws IOException, IllegalAccessException {
+    private static <T> void injectField(InputStream in, Field field, T element) throws IOException {
+        try {
+            field.setAccessible(true);
+            Object fieldValue = readElement(in, field.getType());
+            field.set(element, fieldValue);
+            field.setAccessible(false);
 
-        field.setAccessible(true);
-        if (field.getType().isPrimitive()) {
-            injectPrimitive(in, field, element);
-
-        } else if (field.getType().isArray()) {
-            injectArrayElements(in, field, element);
-
-        } else {
-            injectObject(in, field, element);
-        }
-        field.setAccessible(false);
-    }
-
-    private static <T> void injectPrimitive(InputStream in, Field field, T element)
-            throws IOException, IllegalAccessException {
-
-        Type type = field.getType();
-        DataInputStream dataInputStream = new DataInputStream(in);
-
-
-        if (type.equals(Boolean.TYPE)) {
-            field.set(element, dataInputStream.readBoolean());
-
-        } else if (type.equals(Byte.TYPE)) {
-            field.set(element, dataInputStream.readByte());
-
-        } else if (type.equals(Short.TYPE)) {
-            field.set(element, dataInputStream.readShort());
-
-        } else if (type.equals(Integer.TYPE)) {
-            field.set(element, dataInputStream.readInt());
-
-        } else if (type.equals(Double.TYPE) || type.equals(Float.TYPE) ||
-                type.equals(Character.TYPE) || type.equals(Long.TYPE)) {
-            throw new ProtocolSerializationException("Unsupported type: " + type);
+        } catch (IllegalAccessException ex) {
+            throw new ProtocolSerializationException("something go wrong");
         }
     }
 
-    private static <T> void injectArrayElements(InputStream in, Field field, T element)
-            throws IOException, IllegalAccessException {
-
-        DataInputStream dataInputStream = new DataInputStream(in);
-        int len = dataInputStream.readInt();
-
-        Object arr = Array.newInstance(field.getType().getComponentType(), len);
-
-        for (int i = 0; i < len; i++) {
-            Array.set(arr, i, readElement(in, field.getType().getComponentType()));
-        }
-
-        field.set(element, arr);
-    }
-
-    private static <T> void injectObject(InputStream in, Field field, T element)
-            throws IOException, IllegalAccessException {
-
-        if (field.getType().equals(String.class)) {
-            int len = in.read() << 24 |
-                    in.read() << 16 |
-                    in.read() << 8 |
-                    in.read();
-            byte[] arr = new byte[len];
-
-            if (in.read(arr) != len) {
-                throw new ProtocolSerializationException("Bytes ended");
-            }
-
-            String str = new String(arr, StandardCharsets.UTF_8);
-
-            field.set(element, str);
-        } else {
-
-            Object fieldObj = readElement(in, field.getType());
-            field.set(element, fieldObj);
-        }
-    }
-
-    private static <T> void validate(Class<T> clazz) {
-        if (clazz.isArray()) {
-            throw new IllegalArgumentException("unsupported type: array");
-        }
-    }
+//    private static <T> void injectObject(InputStream in, Field field, T element)
+//            throws IOException, IllegalAccessException {
+//
+//        if (field.getType().equals(String.class)) {
+//            int len = in.read() << 24 |
+//                    in.read() << 16 |
+//                    in.read() << 8 |
+//                    in.read();
+//            byte[] arr = new byte[len];
+//
+//            if (in.read(arr) != len) {
+//                throw new ProtocolSerializationException("Bytes ended");
+//            }
+//
+//            String str = new String(arr, StandardCharsets.UTF_8);
+//
+//            field.set(element, str);
+//        } else {
+//
+//            Object fieldObj = readElement(in, field.getType());
+//            field.set(element, fieldObj);
+//        }
+//    }
 }
