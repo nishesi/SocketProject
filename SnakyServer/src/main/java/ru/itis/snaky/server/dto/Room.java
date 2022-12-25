3512bc0a60026fa048f0af38c9bcf1ccabe3769a
Zl4ve <mudarisov.renat58@gmail.com>
2022-12-26 01:27:18 +0300

    Added DTO

---
+++ SnakyServer/src/main/java/ru/itis/snaky/server/dto/Room.java
@@ -0,0 +1,18 @@
+package ru.itis.snaky.server.dto;
+
+import lombok.*;
+import ru.itis.snaky.server.game.RoomCondition;
+
+@Getter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+    private int size;
+    private String name;
+    private int playersCount;
+    private int capacity;
+    private Color[] colorsArray;
+    private RoomCondition condition;
+}
---
+++ SnakyServer/src/main/java/ru/itis/snaky/server/dto/Snake.java
@@ -0,0 +1,53 @@
+package ru.itis.snaky.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Snake {
+
+    private List<Integer[]> bodyCoordinates;
+    private String snakeName;
+    private Color color;
+
+    private String direction;
+
+    public void move() {
+
+        for (int i = bodyCoordinates.size() - 1; i <= 0; i--) {
+            if (i == 0) {
+                switch (direction) {
+                    case "UP":
+                        bodyCoordinates.set(i, new Integer[]{bodyCoordinates.get(i)[0], bodyCoordinates.get(i)[1] - 1});
+                        break;
+                    case "LEFT":
+                        bodyCoordinates.set(i, new Integer[]{bodyCoordinates.get(i)[0] - 1, bodyCoordinates.get(i)[1]});
+                        break;
+                    case "DOWN":
+                        bodyCoordinates.set(i, new Integer[]{bodyCoordinates.get(i)[0], bodyCoordinates.get(i)[1] + 1});
+                        break;
+                    case "RIGHT":
+                        bodyCoordinates.set(i, new Integer[]{bodyCoordinates.get(i)[0] + 1, bodyCoordinates.get(i)[1]});
+                        break;
+                }
+            } else {
+                bodyCoordinates.set(i, bodyCoordinates.get(i - 1));
+            }
+        }
+    }
+
+    public Integer[] getHead() {
+        return bodyCoordinates.get(0);
+    }
+
+    public void increase() {
+
+    }
+}
