4fd2ea78addb3b7f417b2bd40789c41252075311
Zl4ve <mudarisov.renat58@gmail.com>
2022-12-26 16:55:03 +0300

    Implemented exit listener

--- SnakyServer/src/main/java/ru/itis/snaky/server/listeners/PlayerExitListener.java
+++ SnakyServer/src/main/java/ru/itis/snaky/server/listeners/PlayerExitListener.java
@@ -4,6 +4,8 @@ import ru.itis.snaky.protocol.message.Message;
 import ru.itis.snaky.protocol.message.MessageType;
 import ru.itis.snaky.protocol.message.parameters.CloseParams;
 import ru.itis.snaky.server.core.Connection;
+import ru.itis.snaky.server.dto.Room;
+import ru.itis.snaky.server.dto.Snake;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class PlayerExitListener extends AbstractServerEventListener {
 
     @Override
     public void handle(Connection connection, Message<?> message) {
-
+        Room room = connection.getRoom();
+        for (Snake snake : room.getCondition().getSnakes()) {
+            if (snake.getSnakeName().equals(connection.getPlayerNickname())) {
+                room.getCondition().getSnakes().remove(snake);
+                break;
+            }
+        }
+        room.setPlayersCount(room.getPlayersCount() - 1);
     }
 }
